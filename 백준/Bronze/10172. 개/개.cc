7332b0f49e53ea2c65433fd3ee9847c7a0d474b6
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:15:45 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF INT32_MAX
+// INT32_MIN, INT64_MIN, INT32_MAX, INT64_MAX
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+	fastio;
+
+	cout<<"|\\_/|"<<"\n";
+	cout << "|q p|   /}" << "\n";
+	cout << "( 0 )\"\"\"\\" << "\n";
+	cout << "|\"^\"`    |" << "\n";
+	cout << "||_/=\\\\__|" << "\n";
+
+	return EXIT_SUCCESS;
+}
