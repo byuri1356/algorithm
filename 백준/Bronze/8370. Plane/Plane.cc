77c19fe09ee3fbb4e102a7b45a09c1afe917a58f
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:13:25 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a * b + c * d;
+
+	return EXIT_SUCCESS;
+}
