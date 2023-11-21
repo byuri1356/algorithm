38e63f8f35d1839356f60fa50ad5004bf3a361bb
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:30:13 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	int N;
+	cin >> N;
+
+	if (N) cout << "Leading the Way to the Future";
+	else cout << "YONSEI";
+
+	return EXIT_SUCCESS;
+}
