f5bccd6fbef4dfb477012c4abb88befec06e355c
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:11:37 +0900

    [Bronze IV] Title: ISBN, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+int a, b, c;
+
+int main() {
+
+	fastio;
+
+	cin >> a >> b >> c;
+	cout << "The 1-3-sum is " << 91 + a * 1 + b * 3 + c * 1;
+
+}
