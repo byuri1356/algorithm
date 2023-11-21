a50edc7f47f66dc0aaea0a07849058e3eb213c47
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:38:24 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstring>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	int N;
+	cin >> N;
+	while (N--) {
+		cout << "SciComLove" << "\n";
+	}
+
+	return EXIT_SUCCESS;
+}
