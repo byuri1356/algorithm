f8d3f87ba2a54952325aa6c2e9e13ac499eec68c
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:41:35 +0900

    [Bronze V] Title: Mathematics, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int n, ans = 0; cin >> n;
+
+	while (n--) {
+		int a; cin >> a;
+		ans += a;
+	}
+
+	cout << ans;
+
+}
