24568e2d52c722a4a1f7052af1943aa8ac60dce9
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:31:19 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+
+
+
+int main() {
+
+	fastio;
+
+	cout << "파이팅!!";
+
+	return EXIT_SUCCESS;
+}
