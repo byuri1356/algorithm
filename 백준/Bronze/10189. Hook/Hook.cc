8403fdfe992116b90bd61030187906ff785b3ea8
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:15:52 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF INT32_MAX
+// INT32_MIN, INT64_MIN, INT32_MAX, INT64_MAX
+using namespace std;
+using int64 = int64_t;
+
+
+int main() {
+	fastio;
+
+	cout << "#  # #### #### #  #"<<"\n";
+	cout << "#### #  # #  # # #" << "\n";
+	cout << "#### #  # #  # # #" << "\n";
+	cout << "#  # #### #### #  #" << "\n";
+
+	return EXIT_SUCCESS;
+}
