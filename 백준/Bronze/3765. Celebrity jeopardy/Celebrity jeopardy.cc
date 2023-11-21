6448cf3e586cf2dc081dae97102f7edcbc16e896
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:06:16 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+    string a;
+    while (1) {
+        getline(cin, a);
+        if (cin.eof()) break;
+        cout << a << "\n";
+    }
+
+
+
+
+
+}
