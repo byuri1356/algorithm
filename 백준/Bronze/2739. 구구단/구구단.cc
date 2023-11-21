489ae316bf043e7ccac436aea3e161ac4636e4b8
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:02:58 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF 2147483647
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+	fastio;
+
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= 9; ++i) {
+		cout << N << " * " << i << " = " << N * i << "\n";
+	}
+
+	return EXIT_SUCCESS;
+}
