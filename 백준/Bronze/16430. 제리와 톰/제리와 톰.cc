b9ae50dcc9b61b8a7d82a745709517f408b7d096
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:32:29 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	int a, b;
+	cin >> a >> b;
+
+	cout << b - a << " " << b;
+
+	return EXIT_SUCCESS;
+}
