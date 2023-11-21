a97c65f9d227b23b7bbf961d1c006b9b35e48a25
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:00:23 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF 109876543210
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+	fastio;
+
+	int64 N;
+	cin >> N;
+
+	for (int64 i = N; i > 0; --i) {
+		for (int64 j = i; j <= N; ++j) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	
+
+	return EXIT_SUCCESS;
+}
