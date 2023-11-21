6625b3cae40d61ed2ad83e5a364931662a7719c0
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:12:21 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF INT32_MIN
+// INT32_MIN, INT64_MIN, INT32_MAX, INT64_MAX
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+	fastio;
+	
+	cout << "224" << "\n" << "money1356";
+
+	return EXIT_SUCCESS;
+}
