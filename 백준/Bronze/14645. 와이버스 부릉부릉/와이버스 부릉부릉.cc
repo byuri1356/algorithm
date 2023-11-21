3741179d7e7c02c800f62484f219bf27023adbd2
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:27:34 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include<cstring>
+#include<string>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; ++i) {
+        int a, b;
+        cin >> a >> b;
+    }
+    cout << "비와이";
+   
+    return 0;
+}
