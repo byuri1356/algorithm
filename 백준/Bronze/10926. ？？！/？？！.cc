dfd7f16196dcc54f2fd5edae82ad85c091e8c42c
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:17:47 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF 2147483647
+using namespace std;
+using int64 = int64_t;
+
+string s;
+
+int main() {
+    fastio;
+    
+    cin >> s;
+    cout << s << "??!";
+
+    return EXIT_SUCCESS;
+}
