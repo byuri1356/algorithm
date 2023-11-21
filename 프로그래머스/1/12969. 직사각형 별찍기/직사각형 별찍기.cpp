bf4407ec34fb401e7a5ca49ab21b207fc068fd40
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 17:02:59 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2.58 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+using int64 = int64_t;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    for(int64 i=0 ; i<b;){
+        for(int64 j=0 ; j < a;){
+            cout<<"*";
+            ++j;
+        }
+        cout<<"\n";
+        ++i;
+    }
+    return 0;
+}
