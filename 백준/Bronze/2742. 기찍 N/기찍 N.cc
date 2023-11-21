2e9c986204e6a8457ae9b54dc5edc7bf673cca58
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:03:11 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int N; cin >> N;
+	while (N--) {
+		cout << N + 1 << "\n";
+	}
+
+}
