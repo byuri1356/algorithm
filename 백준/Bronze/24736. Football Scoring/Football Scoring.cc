115499ca0f7e435e0f3543cb87ba3fc80be6f8a5
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:40:13 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<ctime>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	cout << a * 6 + b * 3 + c * 2 + d + e * 2 << ' ';
+	cin >> a >> b >> c >> d >> e;
+	cout << a * 6 + b * 3 + c * 2 + d + e * 2;
+
+}
