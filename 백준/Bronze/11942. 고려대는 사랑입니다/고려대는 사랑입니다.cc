fcf0890f02aba892d8ff0e217d08f379d0de5683
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:22:36 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+int main() {
+
+	fastio;
+
+	cout << "고려대학교";
+
+	return EXIT_SUCCESS;
+}
