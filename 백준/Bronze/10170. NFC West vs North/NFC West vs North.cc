0a0975e8ae1a9522facf48847e69feaf259697be
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:15:33 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	cout << "NFC West       W   L  T" << "\n";
+	cout << "-----------------------" << "\n";
+	cout << "Seattle        13  3  0" << "\n";
+	cout << "San Francisco  12  4  0" << "\n";
+	cout << "Arizona        10  6  0" << "\n";
+	cout << "St. Louis      7   9  0" << "\n";
+	cout << "" << "\n";
+	cout << "NFC North      W   L  T" << "\n";
+	cout << "-----------------------" << "\n";
+	cout << "Green Bay      8   7  1" << "\n";
+	cout << "Chicago        8   8  0" << "\n";
+	cout << "Detroit        7   9  0" << "\n";
+	cout << "Minnesota      5  10  1" << "\n";
+
+	return EXIT_SUCCESS;
+}
