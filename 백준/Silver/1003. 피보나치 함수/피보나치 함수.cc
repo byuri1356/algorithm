2c129842d870c9e6f563205c2afd6f75120c3de7
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 15:48:38 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+
+using namespace std;
+using int64 = int64_t;
+
+pair<int64, int64> p[41];
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	p[0].first = 1;
+	p[0].second = 0;
+	p[1].first = 0;
+	p[1].second = 1;
+
+	for (int64 i = 2; i < 41;) {
+		p[i].first = p[i - 1].first + p[i - 2].first;
+		p[i].second = p[i - 1].second + p[i - 2].second;
+		++i;
+	}
+
+	int64 T,t;
+	cin >> T;
+	for (int64 i = 0; i < T;) {
+		cin >> t;
+		cout << p[t].first  << ' ' << p[t].second << "\n";
+		++i;
+	}
+
+
+
+
+	return EXIT_SUCCESS;
+}
