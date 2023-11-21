ad71f1192cb1db498fff26d49ef502b0760a4cf1
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:36:57 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	fastio;
+
+	cout << 1;
+
+	return EXIT_SUCCESS;
+}
