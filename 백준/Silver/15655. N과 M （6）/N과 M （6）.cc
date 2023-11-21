6829fbf6b17da558b2e81c8b0f5d7317f5e0c862
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:29:36 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+using int64 = int64_t;
+
+vector<int64> v,m;
+
+inline void Solve(int64 A, int64 B, int64 S) {
+	if (S == 0) {
+		for (auto _iter : v) {
+			cout << _iter << ' ';
+		}
+		cout << "\n";
+	}
+	else {
+		for (int64 i = B; i < m.size(); ++i) {
+			v.push_back(m[i]);
+			Solve(A, i + 1, S - 1);
+			v.pop_back();
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int64 T, t, a;
+	cin >> T >> t;
+	for (int64 i = 0; i < T;) {
+		cin >> a;
+		m.push_back(a);
+		++i;
+	}
+	sort(m.begin(), m.end());
+	Solve(T,0, t);
+
+	return EXIT_SUCCESS;
+}
