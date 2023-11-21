717661581ae5094343f8300d66c5058449cac6d8
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:04:15 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int a, b, c, d, e, f, g; cin >> a >> b >> c >> d >> e >> f >> g;
+	cout << c - a * b << " " << d - a * b << " " << e - a * b << " " << f - a * b << " " << g - a * b;
+
+
+
+
+
+}
