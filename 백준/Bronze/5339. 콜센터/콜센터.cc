018946b089bdf229489b3be29d4b3d9e445a561f
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:08:31 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF 109876543210
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+	fastio;
+
+	cout << "     /~\\" << "\n";
+	cout << "    ( oo|" << "\n";
+	cout << "    _\\=/_" << "\n";
+	cout << "   /  _  \\" << "\n";
+	cout << "  //|/.\\|\\\\" << "\n";
+	cout << " ||  \\ /  ||" << "\n";
+	cout << "============" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+	cout << "|          |" << "\n";
+
+
+	return EXIT_SUCCESS;
+}
