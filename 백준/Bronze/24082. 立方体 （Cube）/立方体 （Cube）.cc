51687166447ee6ef3c6c49b86ee759cecd9c9bec
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:39:09 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	ll n;
+	cin >> n;
+	cout << n * n * n;
+
+}
