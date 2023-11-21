43c6b8c699018d9da8adb2ebd8cdaed8a79e8f80
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:22:02 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+#define INF 2147483647
+using namespace std;
+using int64 = int64_t;
+
+int64 arr[1001], _count[1001] = {}, Ans;
+
+int main() {
+	fastio;
+
+	int64 N;
+	cin >> N;
+	for (int64 i = 1; i <= N;) {
+		cin >> arr[i];
+		++i;
+	}
+	for (int64 i = 1; i < N;) {
+		for (int64 j = i + 1; j <= N;) {
+			if (arr[i] > arr[j]) _count[j] = max(_count[j], _count[i] + 1);
+			++j;
+		}
+		++i;
+	}
+
+	Ans = *max_element(_count, _count + N + 1);
+	cout << Ans + 1;
+
+	return EXIT_SUCCESS;
+}
