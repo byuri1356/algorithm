0514deeb22567297c239d7640efb36c42c11863e
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:28:14 +0900

    [Silver I] Title: 스타트와 링크, Time: 276 ms, Memory: 19064 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+static int N, fair = Integer.MAX_VALUE;
+static int[][] ability;
+static boolean[] team;
+
+public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    
+    N = Integer.parseInt(st.nextToken());
+    ability = new int[N+1][N+1];
+    team = new boolean[N+1];
+    
+    for (int i=1; i<=N; i++) {
+        st = new StringTokenizer(br.readLine());
+        for (int j=1; j<=N; j++) {
+            ability[i][j] = Integer.parseInt(st.nextToken());
+        }
+    }
+    combi(1, 0);
+    System.out.println(fair);
+}
+
+static void combi(int start, int depth) {
+    if (depth==N/2) {
+        balance();
+        return;
+    }
+    for (int i=start; i<N; i++) {
+        team[i] = true;
+        combi(i+1, depth+1);
+        team[i] = false;
+    }
+}
+
+static void balance() {
+    int start = 0;
+    int link = 0;
+    for (int i=1; i<=N; i++) {
+        for (int j=1; j<=N; j++) {
+            if (team[i] && team[j]) {
+                start += ability[i][j];
+            } else if (!team[i] && !team[j]) {
+                link += ability[i][j];
+            }
+        }
+    }
+    fair = Math.min(fair, Math.abs(start-link));
+}
+}
