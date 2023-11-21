6a927f2f5e2d71a56ce521de4ba18400df1bfc45
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:05:31 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int a, b;
+	cin >> a >> b;
+	cout << 2 * b - a;
+
+
+
+
+
+}
