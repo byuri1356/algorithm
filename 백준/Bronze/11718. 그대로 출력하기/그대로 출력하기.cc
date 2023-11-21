9d64f8c4dbaa9d98a02b14472b861366108c420c
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:21:55 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+int main() {
+
+	fastio;
+
+	string s;
+	while (getline(cin, s)) {
+		cout << s << "\n";
+	}
+
+	return EXIT_SUCCESS;
+}
