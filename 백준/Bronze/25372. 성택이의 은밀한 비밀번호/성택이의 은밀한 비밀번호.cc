ed77ca0173266a2f3cbbc5735bcc2866633879d8
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:40:58 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+
+
+int main() {
+
+	fastio;
+
+	int N;
+	cin >> N;
+
+	while (N--) {
+		string s;
+		cin >> s;
+		if (s.length() <= 9 && 6 <= s.length()) cout << "yes" << "\n";
+		else cout << "no" << "\n";
+	}
+	
+}
