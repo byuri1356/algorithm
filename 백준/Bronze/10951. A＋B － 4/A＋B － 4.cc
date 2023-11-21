5ca0b2bedcd648ced271984115140f0e56539916
유승민 <97418145+byuri1356@users.noreply.github.com>
2023-11-21 16:18:01 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using int64 = int64_t;
+
+
+
+int main() {
+
+	int a, b;
+	while (cin >> a >> b) {
+		cout << a + b << "\n";
+	}
+
+	return EXIT_SUCCESS;
+}
