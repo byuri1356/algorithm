3e3c8fe0484b5f163deab37e13e1f5d2faff19d0
유승민 <97418145+byuri1356@users.noreply.github.com>
2024-01-04 20:25:06 +0900

    [Bronze IV] Title: Patyki, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8723. Patyki/Patyki.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3];
+bool f1, f2;
+
+int main(){
+    
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a + 3);
+    
+    if(a[0] == a[1] && a[1] == a[2]) f1 = true;
+    if(a[2] * a[2] == a[0] * a[0] + a[1] * a[1]) f2 = true;
+    if(!f1 && !f2) cout << 0;
+    if(!f1 && f2) cout << 1;
+    if(f1 && !f2) cout << 2;
+}
