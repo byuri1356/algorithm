73602364a29c72d309743d530e5b6206030bd87f
유승민 <97418145+byuri1356@users.noreply.github.com>
2024-02-12 16:10:45 +0900

    [Platinum V] Title: 박성원, Time: 568 ms, Memory: 27880 KB -BaekjoonHub

---
+++ 백준/Platinum/1086. 박성원/박성원.cc
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<memory.h>
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF, BUFSIZ);
+using namespace std;
+using ll = long long;
+
+ll n, k, modNum[16], modDigit[51] = { 1, }, dp[1 << 15][101], bunmo = 1, bunza;
+string s[16];
+
+ll sol(ll bt, ll pv) {
+
+	ll& ref = dp[bt][pv];
+	if (ref != -1) return ref;
+
+	if ((1 << n) - 1 == bt) return ref = pv % k == 0;
+
+	ref = 0;
+
+	for (ll i = 0; i < n; ++i) {
+		if ((bt & 1 << i) == 0) ref += sol(bt | 1 << i, (pv * modDigit[s[i].size()] + modNum[i]) % k);
+	}
+
+	return ref;
+}
+
+ll gcd(ll a, ll b) {
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+
+ll getMod(string& s) {
+	ll tmp = 0;
+	for (ll i = 0; i < s.size(); ++i) {
+		tmp *= 10;
+		tmp += s[i] - '0';
+		tmp %= k;
+	}
+	return tmp;
+}
+
+int main() {
+
+	fastio;
+
+	memset(dp, -1, sizeof dp);
+
+	cin >> n;
+	for (ll i = 0; i < n; ++i) cin >> s[i], bunmo *= (i + 1);
+	cin >> k;
+
+	for (ll i = 1; i < 51; ++i) modDigit[i] = modDigit[i - 1] * 10 % k;
+	for (ll i = 0; i < n; ++i) modNum[i] = getMod(s[i]);
+
+	bunza =  sol(0, 0);
+
+	ll divNum = gcd(bunmo, bunza);
+
+	if (bunza == 0) cout << "0/1";
+	else cout << bunza / divNum << "/" << bunmo / divNum;
+
+	//cout << "!";
+}
