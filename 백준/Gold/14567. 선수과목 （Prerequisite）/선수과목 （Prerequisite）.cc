40a68c0de4ce43098e9aab4da18bc88a1ca3563b
유승민 <97418145+byuri1356@users.noreply.github.com>
2024-12-08 23:13:43 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 248 ms, Memory: 4820 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int N, M, dp[1001];
+vector<int> adj[1001];
+
+int main() {
+
+	cin >> N >> M;
+	fill(dp, dp + N + 1, 1);
+	while (M--) {
+		int a, b; cin >> a >> b;
+		adj[a].push_back(b);
+	}
+
+	for (int i = 1; i <= N; ++i) {
+		for (auto& nn : adj[i]) {
+			dp[nn] = dp[nn] < dp[i] + 1 ? dp[i] + 1 : dp[nn];
+		}
+	}
+
+	for (int i = 1; i <= N; ++i) {
+		cout << dp[i] << " ";
+	}
+
+	return 0;
+}
